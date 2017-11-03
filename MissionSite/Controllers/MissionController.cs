7f6217e9e84fced608fc20fdbfb20529ac082aab
gillkyle <kylerobertgill@gmail.com>
2017-11-03 14:09:48 -0600

    project 1 50%

--- MissionSite/Controllers/MissionController.cs
+++ MissionSite/Controllers/MissionController.cs
@@ -13,5 +13,10 @@ namespace MissionSite.Controllers
         {
             return View();
         }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
     }
 }
